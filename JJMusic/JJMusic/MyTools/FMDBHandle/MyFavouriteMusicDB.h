c94c5aebe4e77ed871030a921ad43c558f808077
xianghongjie <coppco@qq.com>
2016-02-19 15:01:05 +0800

    收藏数据库操作

---
+++ JJMusic/JJMusic/MyTools/FMDBHandle/MyFavouriteMusicDB.h
@@ -0,0 +1,13 @@
+//
+//  MyFavouriteMusicDB.h
+//  JJMusic
+//
+//  Created by coco on 16/2/19.
+//  Copyright © 2016年 XHJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyFavouriteMusicDB : NSObject
+
+@end
---
+++ JJMusic/JJMusic/MyTools/FMDBHandle/MyFavouriteMusicDB.m
@@ -0,0 +1,13 @@
+//
+//  MyFavouriteMusicDB.m
+//  JJMusic
+//
+//  Created by coco on 16/2/19.
+//  Copyright © 2016年 XHJ. All rights reserved.
+//
+
+#import "MyFavouriteMusicDB.h"
+
+@implementation MyFavouriteMusicDB
+
+@end
