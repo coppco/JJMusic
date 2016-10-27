613bcaed467a606f980976e0998f94c24ca0b644
coppco <coppco@qq.com>
2016-10-27 08:58:34 +0800

    Swift3.0初始化

---
+++ JJMusic/JJMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JJMusic
+//
+//  Created by coco on 16/10/27.
+//  Copyright © 2016年 XHJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
